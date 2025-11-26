1a14867b33b001c5975b85a45a80f9e726485d5e
wilson nshizirungu <wilsonshizirungu@gmail.com>
2025-11-26 12:24:57 +0200

    SQL_more_queries

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
--- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost)
-
+-- Show privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Show privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
