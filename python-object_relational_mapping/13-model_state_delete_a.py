b9752d932ffa902c4ad57ac6df55f7c1f41d1bbc
wilson nshizirungu <wilsonshizirungu@gmail.com>
2025-11-26 14:55:56 +0200

    python object relational mapping

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Script that deletes all State objects with a name containing the letter a
+from the database hbtn_0e_6_usa
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -19,7 +23,7 @@ if __name__ == "__main__":
         pool_pre_ping=True
     )
 
-    Base.metadata.create_all(engine)
+    Base.metadata.create_all(engine)    
 
     Session = sessionmaker(bind=engine)
     session = Session()
