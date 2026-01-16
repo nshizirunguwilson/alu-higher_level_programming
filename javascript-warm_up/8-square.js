92e5c2b249d3541139ff40cbb4563c87b60fe4b1
wilson nshizirungu <wilsonshizirungu@gmail.com>
2026-01-17 01:37:00 +0200

    development

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,8 +3,8 @@
 const squareSize = parseInt(process.argv[2]);
 
 if (squareSize) {
-  for (let i = 1; i <= squareSize; i++) {
-    console.log('*'.repeat(num));
+  for (let i = 1; i < squareSize; i++) {
+    console.log('*'.repeat(squareSizej));
   }
 } else if (num < 0) {
   console.log();
