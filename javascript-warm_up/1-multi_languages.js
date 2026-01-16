e6db630781cb5b4c424b1a86853317057c5d108a
wilson nshizirungu <wilsonshizirungu@gmail.com>
2026-01-17 00:41:00 +0200

    development

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const cfun = "C is fun";
-const python = "Python is cool";
-const javascript = "JavaScript is amazing";
+const cfun = 'C is fun';
+const python = 'Python is cool';
+const javascript = 'JavaScript is amazing';
 console.log(cfun);
 console.log(python);
 console.log(javascript);
