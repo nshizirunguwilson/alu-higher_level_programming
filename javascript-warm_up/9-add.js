3ba260781378ebcc4b2638d83f4717c0edc50760
wilson nshizirungu <wilsonshizirungu@gmail.com>
2026-01-17 01:42:37 +0200

    development

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const num1 = process.argv[2];
-const num2 = process.argv[3];
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
 
 function add (a, b) {
   return a + b;
